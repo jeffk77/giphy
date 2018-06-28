a412a158e3ec513f82abd5d7ab0c5a5e90a1eda7
jeffk77 <jeff.kushner@me.com>
2018-06-28 18:28:02 -0400

    Added Comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Variable and Array Establishing
 var userText = "";
 var giphys = ["Shawshank Redemption", "Toy Story", "Men in Black", "Ace Ventura", "Avengers", "Short Circuit", "Trading Places", "Coming to America", "Dirty Harry", "Iron Man", "The Expendables", "Terminator", "Judge Dredd", "Star Wars", "Star Trek"];
 var giphysFull = false;
@@ -5,6 +6,7 @@ var giphyImage = "";
 var imageRating = "";
 var ratingText = "";
 
+// Establishing the createButtons function, appending user created buttons based on inputted data, to the giphyButtons class.
 function createButtons() {
     $(".giphyButtons").empty();
     for (var i = 0; i < giphys.length; i++) {
@@ -12,10 +14,12 @@ function createButtons() {
 	}
 }
 
+// addUserText function, used to grab user data, and push it to the userText variable.
 function addUserText() {
     giphys.push(userText);
 }
 
+// emptying user text box after accepting and pushing it to a variable.
 function clearUserText() {
 	userText = "";
 	$(".searchBox").val("");
@@ -23,8 +27,10 @@ function clearUserText() {
 
 $(document).ready(function () {
 
+	// Calling the createButtons function
 	createButtons();
 
+	// The series of functions designed to accept user search entry, create the applicable button, and then clear the entry.
 	$(".addButton").click(function (e) {
 		userText = $(".searchBox").val();
 		if (userText !== "") {
@@ -36,10 +42,13 @@ $(document).ready(function () {
 		}
 	});
 
+	// The response to a user click on a button, either created, or pre-supplied.
 	$(".buttonSection").on("click", ".giphys", function (event) {
 
+		// The variable containing the user's search text.
 		var giphyChoiceText = $(this).text();
 
+		// The url of the GIPHY API, utlizing a combination of my API particulars, and the content of the user search, and finally a limiting of the search results to 10.
 		var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=E3GCgeRKo8GLwMpNuLVdOjzcFCe3UIMD&q=" + giphyChoiceText + "&limit=10&offset=0&rating=G&lang=en";
 
 		$.ajax({
@@ -49,6 +58,7 @@ $(document).ready(function () {
 
 			function giphyGenerate() {
 
+				// This for loop, provides the still images drawn from the GIPHY API.
 				for (var i = 0; i < response.data.length; i++) {
 
 					giphyImage = $("<img class='stillPictures'>");
@@ -86,6 +96,7 @@ $(document).ready(function () {
 			});
 		});
 
+	// The function that allows the GIF's to respond to a click, to animate them, and to still again, if clicked on again.
 	$(document).on("click", ".stillPictures", function (event) {
 		var that = $(this);
 
